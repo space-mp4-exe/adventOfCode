e2237595b675dc85fd73af79019548d8a0854908
space-mp4-exe <brentbristol3@gmail.com>
2026-02-16 13:22:47 -0600

    finished 2025 day 3 part 1

---
+++ 2025/day3/day3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std; 
+
+
+int main(){
+    ifstream input("input.txt");
+    int accumulator;
+    string line;
+    while(getline(input, line)){
+        int max_num = 0;
+        for(int i = 0; i < line.size(); i++){
+            for(int j = i + 1; j < line.size(); j++){
+                if(i == j){
+                    continue;
+                }
+
+                char* ptr;
+                string num = line.substr(i, 1) + line.substr(j,1);
+                int curr = strtol(num.c_str(), &ptr, 10);
+                if(curr > max_num){
+                    max_num = curr;
+                }
+            }
+        }
+        cout << "max_num: " << max_num << endl;
+        accumulator += max_num;
+    }
+    cout << accumulator << endl;
+    return 0;
+}
